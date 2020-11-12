585ca87e2ef334ced5ad0a94fc21fb2f256e4b38
mcembalest <mcembalest@g.harvard.edu>
2020-11-12 18:51:22 -0500

    cleaned up a lot! not passing tests 100% of the time though

--- ad.py
+++ ad.py
@@ -5,24 +5,9 @@
 '''
 
 TODO:
-sin, cos, tan, exp, log
-generator that yields multiple variables
+multiple variables
 
-
-# from GradDog.functions import exp, sin, cos, variables
-
-# x, y = variables(2)
-# #exp, sin, cos are all instances of the Function base class
-
-# f = exp(-(sin(x) - cos(y))^2)
-
-
-
-
-
-
-
-
+trace table (using temporary variables v1, v2, ...)
 # f(x, y) = exp(-(sin(x) - cos(y))^2)
 
 
@@ -46,278 +31,98 @@ import numpy as np
 
 class Function:
 
-	def __init__(self, params):#name, val, upper, left = None, right = None, op = None, der = 1):
-		self.name = params['name']
-		self.val = params['val']
-		self.der = params['der']
+	def __init__(self, name, val, der):
+		# using a dictionary for inputs for now
+		self.name = name
+		self.val = val
+		self.der = der
 
 	def __repr__(self):
 		return self.name
 
 	def __add__(self, other):
 		# self + other
-
 		try:
-
-			new_name = self.name+'+'+other.name
-			new_val = self.val + other.val
-			new_der = self.der + other.der
-	
+			return Function(name = self.name+'+'+other.name, val = self.val + other.val, der = self.der + other.der)
 		except AttributeError:
-
-			new_name = self.name + f'+{other}'
-			new_val = self.val + other
-			new_der = self.der
-
-		params = {
-			'name' : new_name,
-			'val' : new_val,
-			'der' : new_der
-		}
-
-		return Function(params)
+			return Function(name = self.name + f'+{other}', val = self.val + other, der = self.der)
 
 	def __radd__(self, other):
 		# other + self
-
 		try:
-
-			new_name = other.name+'+'+self.name
-			new_val = self.val + other.val
-			new_der = self.der + other.der
-	
+			return Function(name = other.name+'+'+self.name, val = self.val + other.val, der = self.der + other.der)
 		except AttributeError:
-
-			new_name = f'{other}+'+self.name
-			new_val = self.val + other
-			new_der = self.der
-
-		params = {
-			'name' : new_name,
-			'val' : new_val,
-			'der' : new_der
-		}
-
-		return Function(params)
+			return Function(name = f'{other}+'+self.name, val = self.val + other, der = self.der)
 
 	def __sub__(self, other):
 		# self - other
-
 		try:
-
-			new_name = self.name+'-'+other.name
-			new_val = self.val - other.val
-			new_der = self.der - other.der
-	
+			return Function(name = self.name+'-'+other.name, val = self.val - other.val, der = self.der - other.der)
 		except AttributeError:
-
-			new_name = self.name + f'-{other}'
-			new_val = self.val - other
-			new_der = self.der
-
-		params = {
-			'name' : new_name,
-			'val' : new_val,
-			'der' : new_der
-		}
-
-		return Function(params)
+			return Function(name = self.name + f'-{other}', val = self.val - other, der = self.der)
 
 	def __rsub__(self, other):
 		# other - self
-
 		try:
-
-			new_name = other.name+'-'+self.name
-			new_val = other.val - self.val
-			new_der = other.der - self.der
-	
+			return Function(name = other.name+'-'+self.name, val = other.val - self.val, der = other.der - self.der)
 		except AttributeError:
-
-			new_name = f'{other}-'+self.name
-			new_val = other - self.val
-			new_der = -self.der
-
-		params = {
-			'name' : new_name,
-			'val' : new_val,
-			'der' : new_der
-		}
-
-		return Function(params)
+			return Function(name = f'{other}-'+self.name, val = other - self.val, der = -self.der)
 
 	def __mul__(self, other):
 		# self * other
-
 		try:
-
-			new_name = self.name+'*'+other.name
-			new_val = self.val*other.val
-			new_der = self.der*other.val + self.val*other.der
-	
+			return Function(name = self.name+'*'+other.name, val = self.val*other.val, der = self.der*other.val + self.val*other.der)
 		except AttributeError:
-
-			new_name = self.name + f'*{other}'
-			new_val = self.val*other
-			new_der = self.der*other
-
-		params = {
-			'name' : new_name,
-			'val' : new_val,
-			'der' : new_der
-		}
-
-		return Function(params)
+			return Function(name = self.name + f'*{other}', val = self.val*other, der = self.der*other)
 
 	def __rmul__(self, other):
 		# other * self
-
 		try:
-
-			new_name = other.name+'*'+self.name
-			new_val = self.val * other.val
-			new_der = self.der*other.val + self.val*other.der
-	
+			return Function(name = other.name+'*'+self.name, val = self.val*other.val, der = self.der*other.val + self.val*other.der)
 		except AttributeError:
-
-			new_name = f'{other}*' + self.name
-			new_val = self.val * other
-			new_der = self.der * other
-
-		params = {
-			'name' : new_name,
-			'val' : new_val,
-			'der' : new_der
-		}
-
-		return Function(params)
+			return Function(name = f'{other}*' + self.name, val = self.val*other, der = self.der*other)
 
 	def __div__(self, other):
 		# self/other
-
 		try:
-
-			new_name = self.name+'/'+other.name
-			new_val = self.val / other.val
-			new_der = (self.der*other.val - self.val*other.der)/(other.val**2)
-	
+			return Function(name = self.name+'/'+other.name, val = self.val / other.val, der = (self.der*other.val - self.val*other.der)/(other.val**2))
 		except AttributeError:
-
-			new_name = self.name + f'/{other}'
-			new_val = self.val / other
-			new_der = self.der / other
-
-		params = {
-			'name' : new_name,
-			'val' : new_val,
-			'der' : new_der
-		}
-
-		return Function(params)
+			return Function(name = self.name + f'/{other}', val = self.val / other, der = self.der / other)
 
 	def __rdiv__(self, other):
 		# other/self
-
 		try:
-
-			new_name = other.name+'/'+self.name
-			new_val = other.val / self.val
-			new_der = (other.der*self.val - self.der*other.val)/(self.val**2)
-	
+			return Function(name = other.name+'/'+self.name, val = other.val / self.val, der = (other.der*self.val - self.der*other.val)/(self.val**2))
 		except AttributeError:
-
-			new_name = f'{other}/' + self.name
-			new_val = other / self.val
-			new_der = - (self.der*other)/(self.val**2)
-
-		params = {
-			'name' : new_name,
-			'val' : new_val,
-			'der' : new_der
-		}
-
-		return Function(params)
+			return Function(name = f'{other}/' + self.name, val = other / self.val, der = - (self.der*other)/(self.val**2))
 
 	def __neg__(self):
-		
-		new_name = f'-({self.name})'
-		new_val = -self.val
-		new_der = -self.der
-
-		params = {
-			'name' : new_name,
-			'val' : new_val,
-			'der' : new_der
-		}
-
-		return Function(params)
+		# -self
+		return Function(name = f'-({self.name})', val = -self.val, der = -self.der)
 
 	def __pow__(self, other):
-		#currently assumes that other is a scalar, not Function object
-		
-		new_name = f'({self.name})**{other}'
-		new_val = self.val ** other
-		new_der = other*self.val**(other - 1)*self.der
+		# self ** other (exponentiation)
+		# currently assumes that other is a scalar, not a Function object
+		# I currently don't know how to implement the derivative of f ** g when f and g are both function...
+		return Function(name = f'({self.name})**{other}', val = self.val ** other, der = other*self.val**(other - 1)*self.der)
 
-		params = {
-			'name' : new_name,
-			'val' : new_val,
-			'der' : new_der
-		}
-
-		return Function(params)
-
-
-class Variable(Function):
-
-	def __init__(self, name, val):
-		params = {
-			'name' : name,
-			'val' : val,
-			'der' : 1
-		}
-		super().__init__(params)
-
+def variable(name, val):
+	return Function(name = name, val = val, der = 1)
 
 def sin(x):
-	params = {
-		'name' : f'sin({x})',
-		'val' : np.sin(x.val),
-		'der' : np.cos(x.val)*x.der
-		}
-	return Function(params)
+	return Function(name = f'sin({x})', val = np.sin(x.val), der = np.cos(x.val)*x.der)
 
 def cos(x):
-	params = {
-		'name' : f'cos({x})',
-		'val' : np.cos(x.val),
-		'der' : -np.sin(x.val)*x.der
-		}
-	return Function(params)
+	return Function(name = f'cos({x})', val = np.cos(x.val), der = -np.sin(x.val)*x.der)
 
 def tan(x):
-	params = {
-		'name' : f'tan({x})',
-		'val' : np.tan(x.val),
-		'der' : x.der/(np.cos(x.val)**2)
-		}
-	return Function(params)
+	return Function(name = f'tan({x})', val = np.tan(x.val), der = x.der/(np.cos(x.val)**2))
 
 def exp(x):
-	params = {
-		'name' : f'exp({x})',
-		'val' : np.exp(x.val),
-		'der' : np.exp(x.val)*x.der
-		}
-	return Function(params)
+	return Function(name = f'exp({x})', val = np.exp(x.val), der = np.exp(x.val)*x.der)
 
 def log(x):
-	params = {
-		'name' : f'log({x})',
-		'val' : np.log(x.val),
-		'der' : x.der/x.val
-		}
-	return Function(params)
+	return Function(name = f'log({x})', val = np.log(x.val), der = x.der/x.val)
 		
 def display_func(f):
 	print(f, 'evaluated at x = ', x.val)
@@ -325,54 +130,78 @@ def display_func(f):
 	print('Derivative:', f.der)
 
 def test_der(f, f_der):
+	# currently assumes that f is a Function
+	# allows for f_der to be a function or a scalar
+	# distinguishes between exact equality, good approximation, and bad approximation
 
 	try:
-		check = (f.der == f_der.val)
+		f_der_val = f_der.val
 	except AttributeError:
-		check = (f.der == f_der)
-	
-	assert check
-
-
+		f_der_val = f_der
+	try:
+		assert f.der == f_der_val
+		print('Passed deriv test :)')
+	except AssertionError:
+		precision = 1e-15
+		if abs(f.der - f_der_val) < precision:
+			print('close enough')
+		else:
+			print('not good enough')
 
 def random_polynomial_test(x):
+	# creates a giant random polynomial called p
+	# then compares p.der with a manually-computed polynomial derivative p_der
 
-	#efficient numpy random number generator
-	generator = np.random.default_rng()
+	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	# 							create p(x)
 
 	#choose a random polynomial degree in [1, 20)
-	n = generator.choice(np.arange(1,20))
+	n = np.random.choice(np.arange(1,20))
 
 	#generate random floats in (0, 100) for coefficients
 	#size = n + 1 because there is 1 constant term and n poly_terms
-	coefs = 100*generator.random(size = n + 1)
+	coefs = 100*np.random.random(size = n + 1)
+
+	# the exponents for the polynomial are 1, ..., n
 	exps = np.arange(1, n + 1)
 
+	# the polynomial terms are the constant 1 followed by the function objects x, x**2, ..., x**n  
 	poly_terms = np.array([1] + [x**d for d in exps])
-	#constructs the polynomial as a dot product
-	#e.g.
-	# 3 + 4*x + 5*x**2 = np.dot([3, 4, 5], [1, x, x**2])
+
+	#constructs the polynomial as a dot product, e.g., p(x) = 3 + 4*x + 5*x**2 can be re-written as np.dot([3, 4, 5], [1, x, x**2])
 	p = np.dot(coefs, poly_terms)
+	#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 
-	
-	#calculate the new coefficients for the derivative
+	#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+	# 							create p'(x)
+
+	# calculate the new terms for the derivative: 
+	# x**d ---> d*x**(d - 1) for d = 1, ..., n
+
 	#ignore the first coefficient from the original polynomial because it represents the scalar term whose deriv is always 0
 	deriv_coefs = (coefs[1:] * exps)*x.der
 
+	# the exponents should be 1, ..., n - 1 for the derivative
 	deriv_exps = exps[:-1]
 	deriv_poly_terms = np.array([1] + [x**d for d in deriv_exps])
 
 	#again use dot product to make the polynomial
 	p_der = np.dot(deriv_coefs, deriv_poly_terms)
+	#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-	#use the above testing function
-	#which uses an assert statement to make sure that p_der contains the value of the derivative of p
+
 	test_der(p, p_der)
 
 
+x = variable('x', np.pi/3)
 
-x = Variable('x', np.pi/3)
+random_polynomial_test(x)
+random_polynomial_test(sin(x))
+random_polynomial_test(cos(x))
+random_polynomial_test(tan(x))
+random_polynomial_test(exp(x))
+random_polynomial_test(log(x))
 
 
 f = exp(-(sin(x) - cos(x))**2.0)
@@ -382,7 +211,9 @@ f_der = exp(-(sin(x) - cos(x))**2.0)*(-2.0*(sin(x) - cos(x))*(cos(x) + sin(x)))
 
 test_der(f, f_der)
 
-display_func(f)
+random_polynomial_test(f)
+
+
 
 
 
