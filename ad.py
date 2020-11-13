3ba59ead4ca2b785e9da304afb7ac58cc1b415bf
mcembalest <mcembalest@g.harvard.edu>
2020-11-13 10:24:30 -0500

    cleaned up even more

--- ad.py
+++ ad.py
@@ -1,38 +1,16 @@
-# Start writing code here...
-# here goes nothin
-
-
 '''
-
 TODO:
 multiple variables
-
 trace table (using temporary variables v1, v2, ...)
-# f(x, y) = exp(-(sin(x) - cos(y))^2)
-
-
-| Trace | Element operation | Element value | Deriv 1 		   		   | Deriv 2                | [Deriv 1, Deriv 2]
-| ------| -----------       | ------------- | -------- 		   		   | -------                | ----------------
-| x1    | x1      			| pi/2   		| 1    	  		   		   | 0	                    | [1, 0]
-| x2    | x2      			| pi/3   		| 0       		   		   | 1   	                | [0, 1]
-| v1    | sin(x1)   		| 1   			| cos(x1)   	   		   | 0	                    | [0, 0]
-| v2    | cos(x2)      	 	| 1/2   		| 0   			   		   | -sin(x2)               | [0, -sqrt(3)/2]
-| v3    | v1 - v2      	 	| 1/2   		| cos(x1)   	   		   | sin(x2) 	            | [0, sqrt(3)/2]
-| v4    | (v3)^2      	 	| 1/4   		| 2(v3)cos(x1)     		   | 2(v3)sin(x2)           | [0, sqrt(3)/2]
-| v5    | -v4	      	 	| -1/4   		| -2(v3)cos(x1)    		   | -2(v3)sin(x2)          | [0, -sqrt(3)/2]
-| v6    | exp(v5)      	 	| exp(-1/4)   	| -2(v3)cos(x1)exp(v5)     | -2(v3)sin(x2)exp(v5)   | [0, exp(-1/4)sqrt(3)/2]
-
-Therfore the partial derivative of f with respect to x at (pi/2, pi/3) is 0, and 
-the partial derivative of f with respect to y at (pi/2, pi/3) is exp(-1/4)sqrt(3)/2
-
 '''
 
 import numpy as np
 
 class Function:
 
+	
+
 	def __init__(self, name, val, der):
-		# using a dictionary for inputs for now
 		self.name = name
 		self.val = val
 		self.der = der
@@ -43,58 +21,58 @@ class Function:
 	def __add__(self, other):
 		# self + other
 		try:
-			return Function(name = self.name+'+'+other.name, val = self.val + other.val, der = self.der + other.der)
+			return Function(name = f'{self.name}+{other.name}', val = self.val + other.val, der = self.der + other.der)
 		except AttributeError:
-			return Function(name = self.name + f'+{other}', val = self.val + other, der = self.der)
+			return Function(name = f'{self.name}+{other}', val = self.val + other, der = self.der)
 
 	def __radd__(self, other):
 		# other + self
 		try:
-			return Function(name = other.name+'+'+self.name, val = self.val + other.val, der = self.der + other.der)
+			return Function(name = f'{other.name}+{self.name}', val = self.val + other.val, der = self.der + other.der)
 		except AttributeError:
-			return Function(name = f'{other}+'+self.name, val = self.val + other, der = self.der)
+			return Function(name = f'{other}+{self.name}', val = self.val + other, der = self.der)
 
 	def __sub__(self, other):
 		# self - other
 		try:
-			return Function(name = self.name+'-'+other.name, val = self.val - other.val, der = self.der - other.der)
+			return Function(name = f'{self.name}-{other.name}', val = self.val - other.val, der = self.der - other.der)
 		except AttributeError:
-			return Function(name = self.name + f'-{other}', val = self.val - other, der = self.der)
+			return Function(name = f'{self.name}-{other}', val = self.val - other, der = self.der)
 
 	def __rsub__(self, other):
 		# other - self
 		try:
-			return Function(name = other.name+'-'+self.name, val = other.val - self.val, der = other.der - self.der)
+			return Function(name = f'{other.name}-{self.name}', val = other.val - self.val, der = other.der - self.der)
 		except AttributeError:
-			return Function(name = f'{other}-'+self.name, val = other - self.val, der = -self.der)
+			return Function(name = f'{other}-{self.name}', val = other - self.val, der = -self.der)
 
 	def __mul__(self, other):
 		# self * other
 		try:
-			return Function(name = self.name+'*'+other.name, val = self.val*other.val, der = self.der*other.val + self.val*other.der)
+			return Function(name = f'{self.name}*{other.name}', val = self.val*other.val, der = self.der*other.val + self.val*other.der)
 		except AttributeError:
-			return Function(name = self.name + f'*{other}', val = self.val*other, der = self.der*other)
+			return Function(name = f'{self.name}*{other}', val = self.val*other, der = self.der*other)
 
 	def __rmul__(self, other):
 		# other * self
 		try:
-			return Function(name = other.name+'*'+self.name, val = self.val*other.val, der = self.der*other.val + self.val*other.der)
+			return Function(name = f'{other.name}*{self.name}', val = self.val*other.val, der = self.der*other.val + self.val*other.der)
 		except AttributeError:
-			return Function(name = f'{other}*' + self.name, val = self.val*other, der = self.der*other)
+			return Function(name = f'{other}*{self.name}', val = self.val*other, der = self.der*other)
 
 	def __div__(self, other):
 		# self/other
 		try:
-			return Function(name = self.name+'/'+other.name, val = self.val / other.val, der = (self.der*other.val - self.val*other.der)/(other.val**2))
+			return Function(name = f'{self.name}/{other.name}', val = self.val / other.val, der = (self.der*other.val - self.val*other.der)/(other.val**2))
 		except AttributeError:
-			return Function(name = self.name + f'/{other}', val = self.val / other, der = self.der / other)
+			return Function(name = f'{self.name}/{other}', val = self.val / other, der = self.der / other)
 
 	def __rdiv__(self, other):
 		# other/self
 		try:
-			return Function(name = other.name+'/'+self.name, val = other.val / self.val, der = (other.der*self.val - self.der*other.val)/(self.val**2))
+			return Function(name = f'{other.name}+{self.name}', val = other.val / self.val, der = (other.der*self.val - self.der*other.val)/(self.val**2))
 		except AttributeError:
-			return Function(name = f'{other}/' + self.name, val = other / self.val, der = - (self.der*other)/(self.val**2))
+			return Function(name = f'{other}/{self.name}', val = other / self.val, der = - (self.der*other)/(self.val**2))
 
 	def __neg__(self):
 		# -self
@@ -103,7 +81,9 @@ class Function:
 	def __pow__(self, other):
 		# self ** other (exponentiation)
 		# currently assumes that other is a scalar, not a Function object
-		# I currently don't know how to implement the derivative of f ** g when f and g are both function...
+		# I currently don't know how to implement the derivative of f ** g when f and g are both functions...
+		if other == 0:
+			return 1
 		return Function(name = f'({self.name})**{other}', val = self.val ** other, der = other*self.val**(other - 1)*self.der)
 
 def variable(name, val):
@@ -124,13 +104,15 @@ def exp(x):
 def log(x):
 	return Function(name = f'log({x})', val = np.log(x.val), der = x.der/x.val)
 		
-def display_func(f):
+def display_func(f, x):
 	print(f, 'evaluated at x = ', x.val)
 	print(f.val)
 	print('Derivative:', f.der)
 
 def test_der(f, f_der):
-	# currently assumes that f is a Function
+	# tests whether f_der is actually the derivative of f
+
+	# currently assumes that f is a Function object
 	# allows for f_der to be a function or a scalar
 	# distinguishes between exact equality, good approximation, and bad approximation
 
@@ -138,65 +120,71 @@ def test_der(f, f_der):
 		f_der_val = f_der.val
 	except AttributeError:
 		f_der_val = f_der
+	diff = abs(f.der - f_der_val)
 	try:
-		assert f.der == f_der_val
-		print('Passed deriv test :)')
+		assert diff == 0
+		print('Passed deriv test : diff = ', diff)
 	except AssertionError:
-		precision = 1e-15
-		if abs(f.der - f_der_val) < precision:
-			print('close enough')
+		precision = 1e-12
+		if diff < precision:
+			print('close enough : diff = ', diff)
 		else:
-			print('not good enough')
+			print('not good enough : diff = ', diff)
+
+
+########### Demonstration ############
+# create a variable with a name and a value
+x = variable('x', np.pi/3)
+
+# create any combination & composition of elementary functions
+f = exp(-(sin(x) - cos(x))**2.0)
+
+# here I manually write out this function's derivative to test it
+f_der = exp(-(sin(x) - cos(x))**2.0)*(-2.0*(sin(x) - cos(x))*(cos(x) + sin(x)))
+
+test_der(f, f_der)
+
+############# Testing ################
 
 def random_polynomial_test(x):
 	# creates a giant random polynomial called p
 	# then compares p.der with a manually-computed polynomial derivative p_der
 
-	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	# 							create p(x)
-
-	#choose a random polynomial degree in [1, 20)
+	# ~~~~~~~~~~~~~~~~~~~~~~~~~~~create p(x)~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~						
+	# choose a random polynomial degree in [1, 20)
 	n = np.random.choice(np.arange(1,20))
 
-	#generate random floats in (0, 100) for coefficients
-	#size = n + 1 because there is 1 constant term and n poly_terms
+	# generate random floats in (0, 100) for coefficients
+	# size = n + 1 because there is 1 constant term and n poly_terms
 	coefs = 100*np.random.random(size = n + 1)
 
-	# the exponents for the polynomial are 1, ..., n
-	exps = np.arange(1, n + 1)
+	# the exponents for the polynomial are 0, ..., n
+	exps = np.arange(n + 1)
 
-	# the polynomial terms are the constant 1 followed by the function objects x, x**2, ..., x**n  
-	poly_terms = np.array([1] + [x**d for d in exps])
+	# the polynomial terms are the constant 1 followed by the function objects x, x**2, ..., x**n, because x**0 automatically evatuates to the scalar 1 instead of a Function object
+	poly_terms = np.array([x**d for d in exps])
 
-	#constructs the polynomial as a dot product, e.g., p(x) = 3 + 4*x + 5*x**2 can be re-written as np.dot([3, 4, 5], [1, x, x**2])
+	# constructs the polynomial as a dot product, e.g., p(x) = 3 + 4*x + 5*x**2 can be re-written as np.dot([3, 4, 5], [1, x, x**2])
 	p = np.dot(coefs, poly_terms)
 	#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
+	#~~~~~~~~~~~~~~~~~~~~~~~~~~~~create p'(x)~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~							
+	# ignore the first coefficient from the original polynomial because it represents the scalar term whose deriv is always 0
+	deriv_coefs = x.der*((coefs * exps)[1:])
 
-	#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-	# 							create p'(x)
-
-	# calculate the new terms for the derivative: 
-	# x**d ---> d*x**(d - 1) for d = 1, ..., n
-
-	#ignore the first coefficient from the original polynomial because it represents the scalar term whose deriv is always 0
-	deriv_coefs = (coefs[1:] * exps)*x.der
-
-	# the exponents should be 1, ..., n - 1 for the derivative
+	# the exponents should be 0, ..., n - 1 for the derivative
 	deriv_exps = exps[:-1]
-	deriv_poly_terms = np.array([1] + [x**d for d in deriv_exps])
+	deriv_poly_terms = np.array([x**d for d in deriv_exps])
 
-	#again use dot product to make the polynomial
+	# again use dot product to make the polynomial
 	p_der = np.dot(deriv_coefs, deriv_poly_terms)
 	#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
 	test_der(p, p_der)
 
-
-x = variable('x', np.pi/3)
-
+print('Extra testing')
 random_polynomial_test(x)
+random_polynomial_test(f)
 random_polynomial_test(sin(x))
 random_polynomial_test(cos(x))
 random_polynomial_test(tan(x))
@@ -204,14 +192,13 @@ random_polynomial_test(exp(x))
 random_polynomial_test(log(x))
 
 
-f = exp(-(sin(x) - cos(x))**2.0)
 
-#manually write out this function's derivative to test it
-f_der = exp(-(sin(x) - cos(x))**2.0)*(-2.0*(sin(x) - cos(x))*(cos(x) + sin(x)))
 
-test_der(f, f_der)
 
-random_polynomial_test(f)
+
+
+
+
 
 
 
