ccfe1c85be04a43091661b700781230b0c91b5b0
Peyton Benac <pbenac@college.harvard.edu>
2020-11-18 10:16:25 -0500

    added typeerror, base args, more tests

--- code/test_ad_usingFunctionFunction.py
+++ code/test_ad_usingFunctionFunction.py
@@ -1,4 +1,5 @@
 import pytest
+import numpy as np
 from ad_usingFunctionFunction import Variable, Function 
 
 def test_variable_add():
@@ -52,3 +53,21 @@ def test_sin():
     x1 = Variable('x', 3)
     g = sin(x1)
     assert g.der == cos(x1)
+    
+def test_string_input():
+    with pytest.raises(TypeError):
+        x_str = Variable('s', 'CS107')
+        
+def test_log_base2():
+    x = Variable('x', 32)
+    base = 2
+    f = log(x, base=base)
+    assert f.val == 5
+    assert f.der == 1/(x.val * np.log(base))
+    
+def test_exp_base2():
+    x = Variable('x', 5)
+    base = 2
+    f = exp(x, base=base)
+    assert f.val == 32
+    assert f.der == (base**x.val)*np.log(base)
