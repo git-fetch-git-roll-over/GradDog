7a252e74bd8c2d89708be348ec8502b78f8acac5
Guanhua <guanhuashu@gmail.com>
2020-11-18 21:59:17 -0600

    adding more tests and creating .gitignore file

--- tests/test_variable.py
+++ tests/test_variable.py
@@ -2,15 +2,46 @@ import pytest
 import numpy as np
 from forward_mode.variable import Variable
 
+def test_name_reset():
+    x1 = Variable('x', 2)
+    x1.name = 'new_x'
+    assert str(x1) == str(Variable('new_x', 2))
+
+def test_val_reset():
+    x1 = Variable('x', 2)
+    x1.val = 4
+    assert str(x1) == str(Variable('x', 4))
+
+def test_val_reset_error():
+    x1 = Variable('x', 2)
+    with pytest.raises(TypeError):
+        x1.val = 'four'
+
 def test_variable_add():
     x1 = Variable('x', 2)
     x2 = Variable('x', 2)
     assert str(x1 + x2) == str(Variable('x+x', 4, 2))
 
+def test_variable_radd():
+    x1 = 4
+    x2 = Variable('x2', 2)
+    assert str(x1+x2) == str(Variable('x2+4', 6))
+
 def test_variable_sub():
     x1 = Variable('x', 2)
     x2 = Variable('x', 2)
-    assert str(x1 - x2) == str(Variable('x-x', 0, 0))
+    assert str(x1-x2) == str(Variable('x-x', 0, 0))
+
+def test_variable_sub_error():
+    x1 = Variable('x', 4)
+    x2 = 4
+    assert str(x1-4) == str(Variable('x-4', 0))
+
+def test_variable_rsub():
+    x1 = 4
+    x2 = Variable('x', 4)
+    assert str(x1-x2) == str(Variable('-x+4', 0, -1))
+
 
 def test_variable_mul():
     x1 = Variable('x', 2)
@@ -20,7 +51,19 @@ def test_variable_mul():
 def test_variable_div():
     x1 = Variable('x', 2)
     x2 = Variable('x', 2)
-    # assert str(x1 / x2) == str(Variable('x/x', 1, 0))
+    assert str(x1 / x2) == str(Variable('x/x', 1.0, 0))
+
+def test_variable_div_error():
+    x1 = Variable('x', 3)
+    x2 = 3
+    assert str(x1/x2) == str(Variable('x/3', 1, (1/3)))
+
+def test_variable_rdiv():
+    x1 = 3
+    x2 = Variable('x', 3)
+    assert str(x1/x2) == str(Variable('3/x', 1, (-1/3)))
+
+
 
 def test_variable_neg():
     x1 = Variable('x', 2)
