73dec9d838d54c3f209af6bf25fb4b82cbd4d3ad
pbenac <31631522+pbenac@users.noreply.github.com>
2020-11-18 19:37:05 -0500

    Add tests to variable

--- tests/test_variable.py
+++ tests/test_variable.py
@@ -2,7 +2,6 @@ import pytest
 import numpy as np
 from forward_mode.variable import Variable
 
-
 def test_variable_add():
     x1 = Variable('x', 2)
     x2 = Variable('x', 2)
@@ -22,7 +21,6 @@ def test_variable_div():
     x1 = Variable('x', 2)
     x2 = Variable('x', 2)
     # assert str(x1 / x2) == str(Variable('x/x', 1, 0))
-
 
 def test_variable_neg():
     x1 = Variable('x', 2)
@@ -49,30 +47,8 @@ def test_variable_pow():
 def test_polynom():
     x1 = Variable('x', 5)
     f = 3*x1**2 + 2*x1 + 5
-
     assert f.der == 32
-    
-def test_sin():
-    x1 = Variable('x', 3)
-    g = sin(x1)
-    assert g.der == cos(x1).val
 
-    
 def test_string_input():
     with pytest.raises(TypeError):
-        x_str = Variable('s', 'CS107')
-        
-def test_log_base2():
-    x = Variable('x', 32)
-    base = 2
-    f = log(x, base=base)
-    assert f.val == 5
-    assert f.der == 1/(x.val * np.log(base))
-    
-def test_exp_base2():
-    x = Variable('x', 5)
-    base = 2
-    f = exp(x, base=base)
-    assert f.val == 32
-    assert f.der == (base**x.val)*np.log(base)
-
+        x_str = Variable('s', 'hello World')
