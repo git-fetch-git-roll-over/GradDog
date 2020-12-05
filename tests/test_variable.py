0c868fef71f7c09aba86eeb4478ec5544d169d15
Peyton Benac <pbenac@college.harvard.edu>
2020-12-05 13:47:07 -0500

    beginning to fix var tests and definte ui

--- tests/test_variable.py
+++ tests/test_variable.py
@@ -2,16 +2,21 @@ import pytest
 import numpy as np
 from graddog.variable import Variable, get_x, get_xy, get_xyz, function_to_Trace, get_vars
 
-
+def test_val_reset_error():
+    x1 = Variable('x', 2)
+    with pytest.raises(TypeError):
+        x1.val = 'four'
+        
 def test_name_reset():
     x1 = Variable('x', 2)
-    x1.name = 'new_x'
-    assert str(x1) == str(Variable('new_x', 2))
+    x1.name = 'x1'
+    assert x1.name == 'x1'
 
 def test_val_reset():
     x1 = Variable('x', 2)
-    x1._val = 4
-    assert str(x1) == str(Variable('x', 4))
+    x1.val = 4
+    assert x1.val == 4
+
     
 def test_getvars():
     a, b, c = get_vars(['a', 'b', 'c'], [3, 2, 1])
@@ -41,38 +46,61 @@ def test_ValueError_gets():
         x, y, z = get_xyz(len2list)
         x, y = get_xy(len1list)
         a, b, c, d, e = get_vars(['a', 'b', 'c', 'd', 'e'], len2list)
-
-def test_val_reset_error():
+        
+#UI DEFINING COMMENTS IN ALL CAPS
+def test_variable_add():
     x1 = Variable('x', 2)
-    with pytest.raises(TypeError):
-        x1._val = 'four'
+    x2 = Variable('y', 5)
+    f = x1 + x2
+    assert f.val == 7
+    #FORMULA SHOULD BE ACCESSIBLE VIA ATTRIBUTE OF F
+    assert f.formula == 'x + y' # and/or contains 'x' 'x' '+'
+    #IT COULD HAVE A DIFFERENT (hidden?) ATTRIBUTE TO STORE THE TRACE FORMULA/NAME INFO
+    assert f._trace_formula == 'v1 + v2'
+    assert x1._trace_id == v1
+    assert x2._trace_id == v2
+    assert f._trace_id == v3
+    
+def test_variable_radd():
+    x1 = 4
+    x2 = Variable('x', 2)
+    f = 4+ x2 #x1 + 4
+    assert f.val == 6
+    assert f.formula == 'x+4'
+    
+def test_variable_sub():
+    x1 = Variable('x', 2)
+    x2 = Variable('y', 5)
+    f = x2 - x1
+    assert f.val == 3
+    #FORMULA SHOULD BE ACCESSIBLE VIA ATTRIBUTE OF F
+    assert f.formula == 'y - x' # and/or contains 'y' 'x' '-'
+    #IT COULD HAVE A DIFFERENT (hidden?) ATTRIBUTE TO STORE THE TRACE FORMULA/NAME INFO
+    assert f._trace_formula == 'v2 - v1'
+    assert f._trace_id == 'v3'
+    
+def test_variable_sub_error():
+    x1 = Variable('x', 4)
+    x2 = 4
+    f = x2 - x1
+    assert f.val == np.approx(0, abs=1e-6)
+    #FORMULA SHOULD BE ACCESSIBLE VIA ATTRIBUTE OF F
+    assert f.formula == '4 - x' # and/or contains 'y' 'x' '-'
+    #IT COULD HAVE A DIFFERENT (hidden?) ATTRIBUTE TO STORE THE TRACE FORMULA/NAME INFO
+    assert f._trace_formula == '4 - v1'
+    assert f._trace_id == 'v2'
 
-# def test_variable_add():
-#     x1 = Variable('x', 2)
-#     x2 = Variable('x', 2)
-#     assert str(x1 + x2) == str(Variable('x+x', 4, 2))
-
-# def test_variable_radd():
-#     x1 = 4
-#     x2 = Variable('x2', 2)
-#     assert str(x1+x2) == str(Variable('x2+4', 6))
-
-# def test_variable_sub():
-#     x1 = Variable('x', 2)
-#     x2 = Variable('x', 2)
-#     assert str(x1-x2) == str(Variable('x-x', 0, 0))
-
-# def test_variable_sub_error():
-#     x1 = Variable('x', 4)
-#     x2 = 4
-#     assert str(x1-4) == str(Variable('x-4', 0))
-
-# def test_variable_rsub():
-#     x1 = 4
-#     x2 = Variable('x', 4)
-#     assert str(x1-x2) == str(Variable('-x+4', 0, -1))
-
-
+def test_variable_rsub():
+    x1 = Variable('x', 4)
+    x2 = 4
+    f = x1 - x2
+    assert f.val == np.approx(0, abs=1e-6)
+    #FORMULA SHOULD BE ACCESSIBLE VIA ATTRIBUTE OF F
+    assert f.formula == 'x - 4' # and/or contains 'y' 'x' '-'
+    #IT COULD HAVE A DIFFERENT (hidden?) ATTRIBUTE TO STORE THE TRACE FORMULA/NAME INFO
+    assert f._trace_formula == 'v1 - 4'
+    assert f._trace_id == 'v2'
+    
 # def test_variable_mul():
 #     x1 = Variable('x', 2)
 #     x2 = Variable('x', 2)
