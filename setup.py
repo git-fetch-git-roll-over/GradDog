0362d8218a56517696ecf1bdb265cb13d120f61b
Peyton Benac <pbenac@college.harvard.edu>
2020-12-08 11:27:32 -0500

    adding setup.py and whl

---
+++ setup.py
@@ -0,0 +1,28 @@
+import pathlib
+from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+# This call to setup() does all the work
+setup(
+    name="graddog",
+    version="1.3",
+    description="Perform automatic differentiation (final project CS107)",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/git-fetch-git-roll-over/cs107-FinalProject",
+    author="Peyton Benac, Max Cembalest, Seeam Shahid Noor, Ivan Shu",
+    author_email="pbenac@college.harvard.edu",
+    license="MIT",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
+    packages= find_packages(where='graddog'),#["__init__", "compgraph", "functions", "math", "tools", "trace"],
+    include_package_data=True,
+    install_requires=["numpy", "pytest", "codecov", "pytest-cov", "pandas", "numbers", "matplotlib"],
+)
