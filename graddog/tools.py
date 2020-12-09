271f71c2696fb227ec567d35ce21585ffd1e4f83
Peyton Benac <pbenac@college.harvard.edu>
2020-12-09 16:16:02 -0500

    adding demos to tools n redo extrema finder

--- graddog/tools.py
+++ graddog/tools.py
@@ -5,12 +5,33 @@ import matplotlib.pyplot as plt
 import graddog as gd
 from graddog.functions import sin, cos, tan, exp, log
 
+# def fun(x):
+#     return 3*x**4 - 6*x**3
+# xs, ys = plot_derivative(fun, -2, 4)
+
+# xs = linearly spaced array b/n -2 and 4
+# ys = derivative values evaluated @ each x in xs
+
+# def fun2(x):
+#     return x**6 - 10*x**5 + x**4 -7*x**3
+
+# xs, ys = plot_derivative(fun2, -5, 5)
+
+
+# def quadratic(x):
+#     a=4
+#     xoffset = 3
+#     yoffset = 0
+#     return a*(x-xoffset)**2 + yoffset
+
+
+    
 def plot_derivative(function, xmin, xmax, n_pts=100, figsize=(6,6), xlabel='x', ylabel='y', plotTitle='Derivative'):
     '''
     Plot the derivative of a function between xmin and xmax, using n_pts linearly spaced points to evaluate it.
     
     Inputs:
-    function[function]: the function of which you'd like to plot the derivative.
+    function[function]: the function of which you'd like to plot the derivative (must only take single input)
     xmin: lower bound on which to calculate derivative
     xmax: upper bound on which to calculate derivative
     n_pts [int](Default: 100): how many points to use when evaluating derivative (more = better resolution but slower)
@@ -39,6 +60,11 @@ def plot_derivative(function, xmin, xmax, n_pts=100, figsize=(6,6), xlabel='x', 
     plt.show()
     return xs, ys
 
+# xs_EX = find_extrema_firstorder(fun, -2, 4)
+# xs_EX == 0.0
+# x = find_extrema_firstorder(quadratic, -10, 10)
+# x == (2.929292929292929, 3.1313131313131315)
+
 def find_extrema_firstorder(function, xmin, xmax, n_pts=100, tolerance = 1e-10):
     '''
     Locate the point where the derivative is closest to zero on the given interval.
@@ -51,8 +77,9 @@ def find_extrema_firstorder(function, xmin, xmax, n_pts=100, tolerance = 1e-10):
     tolerance [float](Default: 1e-10): how close to zero should a value be before it's considered an extrema?
     
     Outputs:
-    xs: the x values of the extrema
-    ys: the y values of the extrema
+    If it can locate your extrema exactly, it will return only the x value(s) of the extrema.
+    If not it will return:
+    xs: a tuple containing the two x values between which the extrema is located
     '''
     xs = np.linspace(xmin, xmax, num=n_pts)
     diag_mtx = gd.trace(function, xs)    
@@ -60,12 +87,28 @@ def find_extrema_firstorder(function, xmin, xmax, n_pts=100, tolerance = 1e-10):
     ys = []
     for i in range(n_pts):
         ys.append(diag_mtx[i][i]) # append the derivative
+    ys = np.array(ys)    
+
+    zeroidx = np.where(ys < tolerance)[0].astype(int)
+    if len(zeroidx) != 0:
+        return xs[zeroidx]
+    else:
+        decreasingIDX = np.where(ys < tolerance)[0].astype(int)
+        increasingIDX = np.where(ys > tolerance)[0].astype(int)
+
+        if len(decreasingIDX) == 0 or len(increasingIDX) == 0:
+            print(f'No extrema located in the interval {xmin} to {xmax}.')
+            return None
+
+        if decreasingIDX[-1] == increasingIDX[0]-1: # Function goes from decreasing -> increasing
+            print(f'Extrema located between x={xs[decreasingIDX[-1]]} and {xs[increasingIDX[0]]}')
+            return (xs[decreasingIDX[-1]],xs[increasingIDX[0]])
+        elif increasingIDX[-1] == decreasingIDX[0]-1: #Function goes from inc -> dec
+            print(f'Extrema located between x={xs[increasingIDX[-1]]} and {xs[decreasingIDX[0]]}')
+            return (xs[increasingIDX[-1]],xs[decreasingIDX[0]])
+
         
-    idx = np.where(np.abs(ys) < tolerance)[0].astype(int)
-    if len(idx) == 0:
-        print(f'No extrema located in the interval {xmin} to {xmax}')
-        return None
-    return xs[idx], ys[idx]
+# xinc, yinc = find_increasing(quadratic, -10, 10)
 
 def find_increasing(function, xmin, xmax, n_pts=100):
     '''
@@ -87,13 +130,16 @@ def find_increasing(function, xmin, xmax, n_pts=100):
     ys = []
     for i in range(n_pts):
         ys.append(diag_mtx[i][i]) # append the derivative
-        
+    ys = np.array(ys)    
     idx = np.where(ys > 0)[0].astype(int)
     if len(idx) == 0:
         print(f'No increasing values located in the interval {xmin} to {xmax}')
         return None
     return xs[idx], ys[idx]
-  
+
+
+# xdec, ydec = find_decreasing(quadratic, -10, 10)
+
 def find_decreasing(function, xmin, xmax, n_pts=100):
     '''
     Locate the region in a given interval where function is decreasing.
@@ -115,11 +161,14 @@ def find_decreasing(function, xmin, xmax, n_pts=100):
     for i in range(n_pts):
         ys.append(diag_mtx[i][i]) # append the derivative
         
+    ys = np.array(ys)    
     idx = np.where(ys < 0)[0].astype(int)
     if len(idx) == 0:
         print(f'No decreasing values located in the interval {xmin} to {xmax}')
         return None
     return xs[idx], ys[idx]
+
+# xs, functionvalues = plot_with_tangent_line(quadratic, 5,  -10, 10)
 
 def plot_with_tangent_line(function, xtangent, xmin, xmax, n_pts=100, figsize=(6,6), xlabel='x', ylabel='y', plotTitle='Function with tangent line'):
     '''
@@ -144,9 +193,10 @@ def plot_with_tangent_line(function, xtangent, xmin, xmax, n_pts=100, figsize=(6
     xs = np.linspace(xmin, xmax, num=n_pts)
     values = function(xs)
     plt.figure(figsize=figsize)
-    
+    derivativevalue = deriv[0][0]
+    print(f'At the point x={xtangent}, the function has a slope of {derivativevalue}')
     plt.plot(xs, values)
-    plt.plot(xs, deriv*xs)
+    plt.plot(xs, derivativevalue*xs)
     plt.title(plotTitle)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
@@ -154,7 +204,7 @@ def plot_with_tangent_line(function, xtangent, xmin, xmax, n_pts=100, figsize=(6
     plt.show()
     return xs, values
 
-# THIS ONE DOESN'T TOTALLY WORK RIGHT YET
+# THIS ONE DOESN'T TOTALLY WORK RIGHT YET 
 def plot_with_normal_line(function, xnormal, xmin, xmax, n_pts=100, figsize=(6,6), xlabel='x', ylabel='y', plotTitle='Function with normal line'):
     '''
     Plot the a function between xmin and xmax, with a normal line at xnormal, using n_pts linearly spaced points to evaluate it.
@@ -180,10 +230,10 @@ def plot_with_normal_line(function, xnormal, xmin, xmax, n_pts=100, figsize=(6,6
     ynormal = function(xnormal)
 #     print(ynormal)
     derivativevalue = deriv[0][0]
-#     print(derivativevalue)
+    print(f'At the point x={xnormal}, the function has a slope of {derivativevalue}')
     plt.figure(figsize=figsize)
     slope = (-1) /(derivativevalue)
-#     print(slope)
+    print(f'At the point x={xnormal}, the slope of the tangent line is {slope}')
     line = slope*(xs-xnormal) + ynormal
     plt.plot(xs, values)
     plt.plot(xs, line)
