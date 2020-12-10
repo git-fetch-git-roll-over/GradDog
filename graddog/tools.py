c2dc843ca6cb4ced14dc3e84878963c94134aa6e
Peyton Benac <pbenac@college.harvard.edu>
2020-12-10 11:57:21 -0500

    fixing tools and removing normal

--- graddog/tools.py
+++ graddog/tools.py
@@ -88,7 +88,8 @@ def find_extrema_firstorder(function, xmin, xmax, n_pts=100, tolerance = 1e-10):
         ys.append(diag_mtx[i][i]) # append the derivative
     ys = np.array(ys)    
 
-    zeroidx = np.where(ys < tolerance)[0].astype(int)
+    zeroidx = np.where(np.abs(ys) < tolerance)[0].astype(int)
+    print(zeroidx)
     if len(zeroidx) != 0:
         return xs[zeroidx]
     else:
@@ -191,57 +192,15 @@ def plot_with_tangent_line(function, xtangent, xmin, xmax, n_pts=100, figsize=(6
     deriv = gd.trace(function,xtangent)
     xs = np.linspace(xmin, xmax, num=n_pts)
     values = function(xs)
+    ytangent = function(xtangent)
     plt.figure(figsize=figsize)
     derivativevalue = deriv[0][0]
     print(f'At the point x={xtangent}, the function has a slope of {derivativevalue}')
     plt.plot(xs, values)
-    plt.plot(xs, derivativevalue*xs)
+    plt.plot(xs, derivativevalue*(xs-xtangent) + ytangent)
     plt.title(plotTitle)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.xlim(xmin, xmax)
     plt.show()
     return xs, values
-
-# THIS ONE DOESN'T TOTALLY WORK RIGHT YET 
-def plot_with_normal_line(function, xnormal, xmin, xmax, n_pts=100, figsize=(6,6), xlabel='x', ylabel='y', plotTitle='Function with normal line'):
-    '''
-    Plot the a function between xmin and xmax, with a normal line at xnormal, using n_pts linearly spaced points to evaluate it.
-    
-    Inputs:
-    function[function]: the function you'd like to plot.
-    xnormal: value at which you want the normal line to intersect the function
-    xmin: lower bound on which to calculate derivative
-    xmax: upper bound on which to calculate derivative
-    n_pts [int](Default: 100): how many points to use when plotting function (more = better resolution but slower)
-    figsize [tuple](Default: (6,6)): figsize in inches (see matplotlib documentation)
-    xlabel [string](Default: 'x'): Label for x axis of plot
-    ylabel [string](Default: 'y'): Label for y axis of plot
-    plotTitle [string](Default: 'Derivative'): Label for title of plot
-    
-    Outputs: 
-    xs: the array of linearly spaced x values between xmin and xmax
-    ys: the derivative evaluated at the values in xs
-    '''
-    deriv = gd.trace(function,xnormal)
-    xs = np.linspace(xmin, xmax, num=n_pts)
-    values = function(xs)
-    ynormal = function(xnormal)
-#     print(ynormal)
-    derivativevalue = deriv[0][0]
-    print(f'At the point x={xnormal}, the function has a slope of {derivativevalue}')
-    plt.figure(figsize=figsize)
-    slope = (-1) /(derivativevalue)
-    print(f'At the point x={xnormal}, the slope of the tangent line is {slope}')
-    line = slope*(xs-xnormal) + ynormal
-    plt.plot(xs, values)
-    plt.plot(xs, line)
-    plt.title(plotTitle)
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.xlim(xmin, xmax)
-    plt.show()
-    return xs, values
-
-    
-    
